f0e4e8ef0c462b98c9c98030d15b2aab9e0bcd92
unknown <mcmanuel755@gmail.com>
2026-09-25 21:29:45 +0100

    added base git folders

---
+++ src/main/java/com/mcmanuel/StaffRecordChecker/StaffRecordCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.mcmanuel.StaffRecordChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffRecordCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffRecordCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcmanuel/StaffRecordChecker/StaffRecordCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcmanuel.StaffRecordChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffRecordCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
